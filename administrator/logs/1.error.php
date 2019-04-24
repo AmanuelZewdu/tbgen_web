baa5dd98a781c6a5a774cee114c60225161b60ac
azewdu <azewdu87@gmail.com>
2019-04-24 15:57:51 +0300

    first commit of tbgen Aman

---
+++ administrator/logs/1.error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-03-22 14:09:22 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-03-22T14:09:22+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-03-22T14:09:38+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
